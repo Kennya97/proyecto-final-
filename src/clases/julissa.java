8afb1adac51915c2018b90e550ecfb545aa0feda
Julissa749 <salinasjulissa6@gmail.com>
2020-05-15 00:29:04 -0600

    Julissa salinas ya hice el cambio 2 ..

---
+++ src/clases/julissa.java
@@ -0,0 +1,25 @@
+
+package clases;
+
+import java.util.Random;
+
+
+public class julissa {
+    public static void main(String[] args) {
+         float a, b, c;
+
+            Random rnd = new Random();
+
+            a = (rnd.nextFloat() * 10);
+
+            b = (rnd.nextFloat() * 10);
+
+            c = (rnd.nextFloat() * 10);
+
+            System.out.println(a);        
+
+            System.out.println(b);        
+
+            System.out.println(c);   
+    }
+}
